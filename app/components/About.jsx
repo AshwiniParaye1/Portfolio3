6ac1deb4ef6ccf2a23e1d0e5ecc4b4de2b0d3841
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-15 19:36:54 +0530

    added about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div className="">About</div>;
+  return (
+    <div className="">
+      <h1 className="text-xl font-bold mb-4">Ashwini Paraye</h1>
+      <p>
+        I am Ashwini Paraye, a passionate Full Stack Developer with a love for
+        writing, based in the vibrant world of technology. With expertise in
+        both front-end and back-end development, I craft seamless, user-focused
+        web applications. When I’m not coding, I share my knowledge and insights
+        through writing. Let’s collaborate to bring your ideas to life with
+        innovative solutions and engaging content.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import GetInTouch from "./components/GetInTouch";
 
 const Page = () => {
   return (
-    <div className="bg-white text-black flex flex-col pl-96 pt-20 pb-20 justify-between min-h-screen w-full mx-auto p-6 space-y-8">
+    <div className="bg-white text-black flex flex-col pl-96 pr-96 pt-20 pb-20 justify-between min-h-screen w-full mx-auto p-6 space-y-8">
       <div className="w-full  max-w-5xl">
         <About />
       </div>
