856405fdd0c4febeae1754f9dac3f911d15eb9b8
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-16 12:14:02 +0530

    added articles and contact

--- app/components/Articles.jsx
+++ app/components/Articles.jsx
@@ -1,7 +1,61 @@
+import Link from "next/link";
 import React from "react";
 
 const Articles = () => {
-  return <div>Articles</div>;
+  return (
+    <div>
+      <h1 className="text-xl font-semibold mb-2">Articles</h1>
+      <li>
+        <span className="text-blue-500 hover:text-blue-700">
+          <Link
+            href="https://peerlist.io/ashwini_paraye/articles/boost-your-development-speed-with-golang"
+            target="_blank"
+          >
+            Boost Your Development Speed with Golang
+          </Link>
+        </span>
+      </li>
+      <li>
+        <span className="text-blue-500 hover:text-blue-700">
+          <Link
+            href="https://blog.stackademic.com/step-by-step-breakdown-of-how-golang-behaves-behind-the-scenes-eba1d5582b55"
+            target="_blank"
+          >
+            How Golang Behaves Behind the Scenes
+          </Link>
+        </span>
+      </li>
+      <li>
+        <span className=" text-blue-500 hover:text-blue-700">
+          <Link
+            href="https://blog.stackademic.com/what-does-enumerable-mean-in-javascript-explained-simply-b3e52d00f621"
+            target="_blank"
+          >
+            What Does Enumerable Mean in JavaScript?
+          </Link>
+        </span>
+      </li>
+      <li>
+        <span className=" text-blue-500 hover:text-blue-700">
+          <Link
+            href="https://blog.stackademic.com/understanding-the-basics-of-next-js-for-beginners-b2e542c3e6e1"
+            target="_blank"
+          >
+            Understanding the Basics of Next.js
+          </Link>
+        </span>
+      </li>
+      <p className="mt-2 pl-5">
+        Check out more of my articles on{" "}
+        <span className="text-blue-500 hover:text-blue-700">
+          <Link href="https://medium.com/@ashwini-paraye" target="_blank">
+            Medium
+          </Link>
+        </span>{" "}
+        to dive deeper into my work and insights!
+      </p>
+    </div>
+  );
 };
 
 export default Articles;
--- app/components/GetInTouch.jsx
+++ app/components/GetInTouch.jsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 const GetInTouch = () => {
-  return <div>GetInTouch</div>;
+  return (
+    <div>
+      <h1 className="text-xl font-semibold mb-2">Get In Touch</h1>
+      <p>
+        Interested in a conversation? Drop DM's over
+        <span className="text-blue-500 hover:text-blue-700">
+          <Link href="https://www.linkedin.com/in/ashwini-paraye/">
+            {" "}
+            linkedin{" "}
+          </Link>
+        </span>
+        or{" "}
+        <span className="text-blue-500 hover:text-blue-700">
+          <Link href="mailto:ashwiniparaye1@gmail.com"> email. </Link>
+        </span>
+        Ask me anything about my work, my projects, or anything else you'd like.
+      </p>
+    </div>
+  );
 };
 
 export default GetInTouch;
